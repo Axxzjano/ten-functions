dce8cd1b799eacfedc3d6f967c53655477ac3da6
Axxzjavon Sampson <axxzjano@gmail.com>
2023-11-15 13:18:33 -0600

    completed ten functions excercise

--- solutions.js
+++ solutions.js
@@ -21,3 +21,58 @@
  * concat
  */
 
+// Question 1
+
+
+function isTrue(input){
+    return input === true;
+}
+
+// Question 2
+function isFalse(value){
+    return  value === false;
+}
+
+// Question 3
+
+function not(input){
+    return !input;
+}
+
+// Question 4
+
+function addOne(number) {
+   return ++number;
+}
+// Question 5
+function isEven(number){
+    return number % 2 === 0;
+}
+
+// Question 6
+function isIdentical(number1, number2){
+    return number1 === number2;
+}
+
+// Question 7
+function isEqual(number1, number2){
+    return number1 == number2;
+}
+
+
+// Question 8
+function or(number1, number2){
+    return number1 || number2;
+}
+
+
+// Question 9
+function and(number1, number2){
+    return number1 && number2;
+}
+
+
+// Question 10
+function concat(number1, number2){
+    return String(number1) + String(number2);
+}
